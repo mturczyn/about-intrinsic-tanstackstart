a0e5e227b8efd43ce5b8a80f687613dedfbfc412
Michal Turczyn <turek1992@o2.pl>
2025-09-30 10:43:05 +0200

    Cleaned initial examples of TanStack Start.

--- src/routes/initial-example.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { getCount } from '../api/counterApi'
-import { InitialExample } from '../components/InitialExample'
-
-export const Route = createFileRoute('/initial-example')({
-    component: InitialExample,
-    loader: async () => await getCount(),
-})
