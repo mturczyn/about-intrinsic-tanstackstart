e69dad7dfdd8a6049da3a36d0cc75b282b87747d
Michal Turczyn <turek1992@o2.pl>
2025-10-19 13:19:15 +0200

    Adjustments in order to deploy to cloudflare.

--- src/api/counterApi.ts
+++ src/api/counterApi.ts
@@ -4,9 +4,12 @@ import * as fs from 'node:fs'
 const filePath = 'count.txt'
 
 async function readCount() {
-    return parseInt(
-        await fs.promises.readFile(filePath, 'utf-8').catch(() => '0')
-    )
+    try {
+        const fileContent = await fs.promises.readFile(filePath, 'utf-8')
+        return parseInt(fileContent)
+    } catch {
+        return 0
+    }
 }
 
 export const getCount = createServerFn({
