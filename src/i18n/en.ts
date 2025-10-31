bceb4010e95a1629b0e4418d15c6a7a0df312d1b
Michal Turczyn <turek1992@o2.pl>
2025-10-31 13:53:14 +0100

    Corrected translation for home page

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -25,7 +25,7 @@ export const en = {
                         experience other .NET frameworks, such as ASP.NET (Core, Razor Pages) or WPF. I also leanred JS along the way (HTML, CSS, JS)
                         and React framework.`,
         fourthPara:
-            '<0>I try to constantly learn and improve. This website is now built with the TanStack Start framework. Previously, I created <1>a version using Next.js</1> to explore React Server Components, and also built <1>a standalone React SPA</1> without server-side rendering.</0>',
+            '<0>I try to constantly learn and improve. This website is now built with the TanStack Start framework. Previously, I created <0>a version using Next.js</0> to explore React Server Components, and also built <1>a standalone React SPA</1> without server-side rendering.</0>',
     },
     pastEmployers: 'Past employers',
     Poland: 'Poland',
