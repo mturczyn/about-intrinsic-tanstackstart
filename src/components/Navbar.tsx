eb07194f89e069301cbd1f9986f7e7e990ce4304
Michal Turczyn <turek1992@o2.pl>
2025-11-26 13:59:59 +0100

    Improved handling of languages: implemented updating route upon language change.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,30 @@
 import plFlag from '@/images/flags/pl.svg'
 import gbFlag from '@/images/flags/gb.svg'
 import { SUPPORTED_LANGUAGES } from '@/i18n'
-import { Link } from '@tanstack/react-router'
+import { Link, useLocation, useNavigate } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next'
+import { validateLocale } from '@/i18n/validateLocale'
 
 export default function Navbar() {
     const { i18n, t } = useTranslation()
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const handleLanguageChange = (newLocale: string) => {
+        const [_, ...path] = location.pathname.slice(1).split('/')
+
+        // Validate the new locale, default to 'en' if invalid
+        if (!validateLocale(newLocale)) {
+            newLocale = 'en'
+        }
+
+        navigate({ to: `/${newLocale}/${path.join('/')}`, replace: true })
+        // Also update i18n
+        i18n.changeLanguage(newLocale)
+        // Navigation + language change should handle it, no need to invalidate
+        // router comes from useRouter hook
+        // router.invalidate()
+    }
 
     return (
         <nav className="flex items-center gap-3 justify-end p-2">
@@ -13,7 +32,7 @@ export default function Navbar() {
             <Link to="/{-$locale}/contact-info">{t('Contact info')}</Link>
 
             <button
-                onClick={() => i18n.changeLanguage(SUPPORTED_LANGUAGES.en)}
+                onClick={() => handleLanguageChange(SUPPORTED_LANGUAGES.en)}
                 className="w-10 h-10 rounded-full overflow-hidden border border-gray-300 hover:scale-105 transition-transform ml-auto"
                 title="English"
             >
@@ -25,7 +44,7 @@ export default function Navbar() {
             </button>
 
             <button
-                onClick={() => i18n.changeLanguage(SUPPORTED_LANGUAGES.pl)}
+                onClick={() => handleLanguageChange(SUPPORTED_LANGUAGES.pl)}
                 className="w-10 h-10 rounded-full overflow-hidden border border-gray-300 hover:scale-105 transition-transform"
                 title="Polski"
             >
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,10 +4,6 @@ import {
     createRootRoute,
     HeadContent,
     Scripts,
-    useRouter,
-    Link,
-    useLocation,
-    useNavigate,
 } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next'
 import i18n, { setSsrLanguage } from '@/i18n'
@@ -73,29 +69,6 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
     const { i18n } = useTranslation()
-    const router = useRouter()
-
-    // const location = useLocation()
-    // const navigate = useNavigate()
-
-    const handleLanguageChange = (lang: string) => {
-        // const [language, ...path] = location.pathname.slice(1).split('/')
-        // console.log('location', location)
-        // console.log('lang', lang)
-        // console.log('language', language)
-        // console.log('path', path)
-        // console.log('`/${lang}/${path.join(/)}`', `/${lang}/${path.join('/')}`)
-
-        // navigate({ to: `/${lang}/${location.pathname}`, replace: true })
-        router.invalidate()
-    }
-
-    useEffect(() => {
-        i18n.on('languageChanged', handleLanguageChange)
-        return () => {
-            i18n.off('languageChanged', handleLanguageChange)
-        }
-    }, [router])
 
     return (
         <html lang={i18n.language}>
